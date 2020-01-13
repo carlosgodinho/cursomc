3b1e7b98ee962ff4bb1551c6adf719c99616de33
Carlos Godinho <carlos.godinho@hotmail.com>
2020-01-13 11:32:28 -0300

    Banco de dados h2 e criacao automatica da base de dados

--- src/main/java/com/carlosgodinho/cursomc/domain/Categoria.java
+++ src/main/java/com/carlosgodinho/cursomc/domain/Categoria.java
@@ -2,9 +2,19 @@ package com.carlosgodinho.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	//anotação do atributo no caso ID
+	@Id
+	//estratégia de geração automática dos id's das categorias
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
